6a2ed1289332c5fba8af1c91547280142c2e1fda
BeeBeeCue <kristoferknutsen@hotmail.com>
2020-12-09 18:58:47 +0200

    nav fixed

--- resources/scripts/buttons.js
+++ resources/scripts/buttons.js
@@ -1,11 +1,13 @@
 var advancedMenuIsOpen = false;
 var basicMenuIsOpen = false;
 
-
-
 document.getElementById("advancedButton").addEventListener("click", advancedMenu);
 document.getElementById("hamburger").addEventListener("click", basicMenu);
 
+function setNav() {
+  document.getElementById("nav").style.transform = "translateX(-500px)"
+  //document.getElementById("nav").style.width = 0;
+}
 
 /*
     ADVANCED MENU / DASHBOARD
@@ -19,13 +21,14 @@ function advancedMenu() {
 }
 
 function openAdvancedMenu() {
-  document.getElementById("nav").style.width = "100%";
+  //document.getElementById("nav").style.width = "100%";
+  document.getElementById("nav").style.transform = "translateX(0)"
   document.getElementById("advancedButton").innerText = "Basic";
   advancedMenuIsOpen = true;
 }
 
 function closeAdvancedMenu() {
-  document.getElementById("nav").style.width = "500px";
+  document.getElementById("nav").style.transform = "translateX(-70%)"
   document.getElementById("advancedButton").innerText = "Advanced";
   advancedMenuIsOpen = false;
 }
@@ -35,25 +38,30 @@ function closeAdvancedMenu() {
 */
 
 function basicMenu() {
-if (basicMenuIsOpen) {
+if (advancedMenuIsOpen)
+{closeAdvancedMenu()}
+else {
+  if (basicMenuIsOpen) {
     closeBasicMenu();
   } else {
     openBasicMenu();
   }
 }
+}
 
 function openBasicMenu() {
 console.log("open");
-document.getElementById("nav").style.transform = "translateX(0px)"
+//document.getElementById("nav").style.width = "30%";
+document.getElementById("nav").style.transform = "translateX(-70%)"
 basicMenuIsOpen = true;
 }
 
 function closeBasicMenu() {
 console.log("close");
-document.getElementById("nav").style.transform = "translateX(-500px)"
+//document.getElementById("nav").style.width = "0";
+document.getElementById("nav").style.transform = "translateX(-100%)"
 basicMenuIsOpen = false;
 }
-
 
 /*
     NICE LOGGER YES
